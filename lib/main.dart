0d7d5bcd8eeca9e7fc4c6fa059a3d078cbb00204
Banno Taichi <taichib@icloud.com>
2024-07-08 17:01:57 +0900

    Refactor PersonCounter Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:utip/widgets/person_counter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -115,19 +116,11 @@ class _UTipState extends State<UTip> {
                         "Split",
                         style: theme.textTheme.titleMedium,
                       ),
-                      Row(
-                        children: [
-                          IconButton(
-                              color: theme.colorScheme.primary,
-                              onPressed: decrement,
-                              icon: Icon(Icons.remove)),
-                          Text("$_personCount",
-                              style: theme.textTheme.titleMedium),
-                          IconButton(
-                              color: theme.colorScheme.primary,
-                              onPressed: increment,
-                              icon: Icon(Icons.add)),
-                        ],
+                      PersonCounter(
+                        theme: theme,
+                        personCount: _personCount,
+                        onDecrement: decrement,
+                        onIncrement: increment,
                       ),
                     ],
                   ),
