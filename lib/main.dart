2e18518484befd56d20da84d172388aa28225173
Banno Taichi <taichib@icloud.com>
2024-07-08 23:25:29 +0900

    Refactor Bill Amount Field

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:utip/widgets/person_counter.dart';
 
+import 'widgets/bill_amount_field.dart';
 import 'widgets/tip_slider.dart';
 
 void main() {
@@ -94,22 +95,17 @@ class _UTipState extends State<UTip> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Container(
-              width: 100,
-              height: 300,
+              padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(5),
                 border: Border.all(color: theme.colorScheme.primary, width: 2),
               ),
               child: Column(
                 children: [
-                  TextField(
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.attach_money),
-                        labelText: "Bill Amount"),
-                    keyboardType: TextInputType.number,
-                    onChanged: (String value) {
-                      print("Value: $value");
+                  BillAmountField(
+                    billAmount: "100",
+                    onChanged: (value) {
+                      print("Amount: $value");
                     },
                   ),
                   // Split Bill area
@@ -144,9 +140,11 @@ class _UTipState extends State<UTip> {
                   TipSlider(
                     tipPercentage: _tipPercentage,
                     onChanged: (double value) {
-                      setState(() {
-                        _tipPercentage = value;
-                      });
+                      setState(
+                        () {
+                          _tipPercentage = value;
+                        },
+                      );
                     },
                   )
                 ],
